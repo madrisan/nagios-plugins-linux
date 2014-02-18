98b2bd6f9b24d90abec9a8407a4db37c343f7b6a
Davide Madrisan <davide.madrisan@gmail.com>
2014-02-18 18:17:51 +0100

    Remove unneeded header files (on Linux)
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- src/check_uptime.c
+++ src/check_uptime.c
@@ -31,30 +31,8 @@
 #include <sys/sysinfo.h>
 #endif
 
-#if TIME_WITH_SYS_TIME
-#include <sys/time.h>
 #include <time.h>
-#else
-#if HAVE_SYS_TIME_H
-#include <sys/time.h>
-#else
-#include <time.h>
-#endif
-#endif
-
 #include <unistd.h>
-
-#ifdef HAVE_SYS_PARAM_H
-#include <sys/param.h>
-#endif
-
-#if HAVE_SYS_SYSCTL_H
-#include <sys/sysctl.h>
-#endif
-
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-#endif
 
 #include "common.h"
 #include "error.h"
