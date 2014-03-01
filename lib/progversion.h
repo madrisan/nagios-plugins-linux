bd8a7b0f5d867259019424a7e3a6a32efe658288
Davide Madrisan <davide.madrisan@gmail.com>
2014-03-01 20:17:01 +0100

    Forgot to add the header file 'progversion.h'
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

---
+++ lib/progversion.h
@@ -0,0 +1,16 @@
+#ifndef _PROGVERSION_H
+#define _PROGVERSION_H
+
+#include "config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static const char *program_version = PACKAGE_VERSION;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PROGVERSION_H */
