3a0dfd399e7cfa698a18a71d0ed0bf51cd7f56ff
Davide Madrisan <davide.madrisan@gmail.com>
2014-03-06 10:37:53 +0100

    Fix license and description
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- lib/meminfo.c
+++ lib/meminfo.c
@@ -1,8 +1,8 @@
 /*
- * License: GPLv2
+ * License: GPLv3+
  * Copyright (c) 2014 Davide Madrisan <davide.madrisan@gmail.com>
  *
- * A Nagios plugin to check memory and swap usage on linux
+ * A library for checking memory and swap usage on linux
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * This software is based on the source code of the tool "free" (procps 3.2.8)
+ * This module is based on the code of procps 3.2.8
  */
 
 #include "config.h"
