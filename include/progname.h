95c3473fa6d411d74ea9d19925d4454102a795db
Davide Madrisan <davide.madrisan@gmail.com>
2014-03-22 12:07:25 +0100

    progname - new exported variable: program_name_short
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- include/progname.h
+++ include/progname.h
@@ -12,6 +12,9 @@ extern "C" {
 /* String containing name the program is called with.  */
 extern const char *program_name;
 
+/* String containing a short version of 'program_name'.  */
+extern const char *program_name_short;
+
 /* Set program_name, based on argv[0].
    argv0 must be a string allocated with indefinite extent, and must not be
    modified after this call.  */
--- lib/progname.c
+++ lib/progname.c
@@ -8,14 +8,16 @@
    To be initialized by main().  */
 const char *program_name = NULL;
 
+/* This string can be used in the final output message */
+const char *program_name_short = NULL;
+
 /* Set program_name, based on argv[0].
    argv0 must be a string allocated with indefinite extent, and must not be
    modified after this call.  */
 void
 set_program_name (const char *argv0)
 {
-  const char *slash;
-  const char *base;
+  const char *slash, *underscore;
 
   if (argv0 == NULL)
     {
@@ -25,7 +27,8 @@ set_program_name (const char *argv0)
     }
 
   slash = strrchr (argv0, '/');
-  base = (slash != NULL ? slash + 1 : argv0);
+  program_name = (slash != NULL ? slash + 1 : argv0);
 
-  program_name = base;
+  underscore = strchr (program_name, '_');
+  program_name_short = (underscore != NULL ? underscore + 1 : program_name);
 }
