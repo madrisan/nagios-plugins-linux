e9250d31deecf986b4535fd3ca648fa0de10e4b7
Davide Madrisan <davide.madrisan@gmail.com>
2014-04-09 22:36:29 +0200

    Add some missing file preambles
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- lib/messages.c
+++ lib/messages.c
@@ -1,3 +1,23 @@
+/*
+ * License: GPLv3+
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This library was based on and inspired by the gnulib code.
+ */
+
 #include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
