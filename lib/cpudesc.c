62a334487fd50032189613d524523dfe71200e69
Davide Madrisan <davide.madrisan@gmail.com>
2014-05-27 23:14:10 +0200

    cpudesc lib - fix warning: ‘get_processor_kernel_max’ defined but not used
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- lib/cpudesc.c
+++ lib/cpudesc.c
@@ -74,11 +74,11 @@ get_processor_number_online (void)
 }
 
 /* Get the maximum cpu index allowed by the kernel configuration. */
-static unsigned int
-get_processor_kernel_max ()
-{
-  return sysfsparser_getvalue (PATH_SYS_CPU "/kernel_max") + 1;
-}
+//static unsigned int
+//get_processor_kernel_max ()
+//{
+//  return sysfsparser_getvalue (PATH_SYS_CPU "/kernel_max") + 1;
+//}
 
 bool
 get_processor_is_hot_pluggable (unsigned int cpu)
