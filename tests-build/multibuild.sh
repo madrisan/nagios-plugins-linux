4de942c84af50874e0349debdd65334711e1f498
Davide Madrisan <davide.madrisan@gmail.com>
2016-10-22 21:11:09 +0200

    tests-build: improve the tests and create the rpm packages
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- tests-build/multibuild.sh
+++ tests-build/multibuild.sh
@@ -6,7 +6,7 @@ PROGNAME="${0##*/}"
 PROGPATH="${0%/*}"
 REVISION=1
 
-die () { echo "$PROGNAME: error: $1" 1>&2; exit 1; }
+die () { echo -e "$PROGNAME: error: $1" 1>&2; exit 1; }
 msg () { echo "*** info: $1"; }
 
 docker_helpers="$PROGPATH/docker-shell-helpers/docker-shell-helpers.sh"
@@ -16,7 +16,8 @@ docker_helpers="$PROGPATH/docker-shell-helpers/docker-shell-helpers.sh"
 
 usage () {
    cat <<__EOF
-Usage: $PROGNAME -o <os> -s <disk> [-d <distro>]
+Usage: $PROGNAME -o <os> -s <shared> [-d <distro>] [--spec <file>] \
+[-t <folder>]
        $PROGNAME --help
        $PROGNAME --version
 
@@ -24,9 +25,13 @@ Where:
    -d | --distro : distribution name (default: no distribution set)
    -o | --os     : distribution (example: centos:centos6)"
    -s | --shared : shared folder that will be mounted on the docker instance
+        --spec   : the specfile to be used for building the rpm packages
+   -t | --target : the directory where to copy the rpm packages
 
 Example:
-       $0 -s $PROGPATH/../../nagios-plugins-linux:/shared:rw -o centos:latest
+       $0 -s $PROGPATH/../../nagios-plugins-linux:/shared:rw \\
+          --spec specs/nagios-plugins-linux.spec \\
+          -t pcks -d mamba -o centos:latest
 
 __EOF
 }
@@ -44,6 +49,8 @@ __EOF
 usr_os=
 usr_disk=
 usr_distro=
+usr_specfile=
+usr_targetdir=
 
 while test -n "$1"; do
    case "$1" in
@@ -57,6 +64,10 @@ while test -n "$1"; do
          usr_os="$2"; shift ;;
       --shared|-s)
          usr_disk="$2"; shift ;;
+      --spec)
+         usr_specfile="$2"; shift ;;
+      --target|-t)
+         usr_targetdir="$2"; shift ;;
       --*|-*) die "unknown argument: $1" ;;
       *) die "unknown option: $1" ;;
     esac
@@ -65,6 +76,8 @@ done
 
 [ "$usr_disk" ] || { usage; exit 1; }
 [ "$usr_os" ] || { usage; exit 1; }
+[ "$usr_specfile" ] &&
+ { [ -r "$usr_specfile" ] || die "no such file: $usr_specfile"; }
 
 # parse the shared disk string
 IFS_save="$IFS"
@@ -75,6 +88,24 @@ IFS="$IFS_save"
 
 ([ "$shared_disk_host" ] && [ "$shared_disk_container" ]) ||
    die "bad syntax for --shared"
+
+
+if [ "$usr_specfile" ]; then
+   specfile="$(readlink -f $usr_specfile)"
+   case "$specfile" in
+      ${shared_disk_host}*)
+          specfile="./${specfile#$shared_disk_host}" ;;
+      *) die "the specfile must be in $shared_disk_host" ;;
+   esac
+fi
+if [ "$usr_targetdir" ]; then
+   targetdir="$(readlink -f $usr_targetdir)"
+   case "$targetdir" in
+      ${shared_disk_host}*)
+         targetdir="$shared_disk_container/${targetdir#$shared_disk_host}" ;;
+      *) die "the target dir must be in $shared_disk_host" ;;
+   esac
+fi
 
 msg "instantiating a new container based on $usr_os ..."
 
@@ -100,32 +131,52 @@ echo "\
 Container \"$container\"  status:running  ipaddr:$ipaddr  os:$os
 "
 
-msg "buiding the packages inside the container $container ..."
+msg "testing the build process inside $container ..."
 container_exec_command "$container" "\
-
-# installing packages require administrative permissions
+# install the build prereqs
 yum install -y bzip2 make gcc xz rpm-build
 
-# create a non-root user (developer)
+# create a non-root user for building the software (developer) ...
 useradd -c 'Developer' developer
 
-# switch to the user developer
+# ... and switch to this user
 su - developer -c '
+msg () { echo \"*** info: \$1\"; }
+
 cp -pr '"$shared_disk_container"/*' .
 ./configure
 make dist
 
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-cp -p tests-build/${pckname}.spec ~/rpmbuild/SPECS/
-cp -p *.tar.* ~/rpmbuild/SOURCES/
+if [ \"'$specfile'\" ]; then
+   mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+   cp -p '$specfile' ~/rpmbuild/SPECS/
+   cp -p ${pckname}*.tar.* ~/rpmbuild/SOURCES/
 
-pushd ~/rpmbuild/SPECS/ &>/dev/null
-rpmbuild \
-   --define=\"dist $rpm_dist\" \
-   --define=\"_topdir \$HOME/rpmbuild\" \
-   -ba ${pckname}.spec
-popd &>/dev/null
+   msg \"creating the rpm packages ...\"
+   pushd ~/rpmbuild/SPECS/ &>/dev/null
+   rpmbuild \
+      --define=\"dist $rpm_dist\" \
+      --define=\"_topdir \$HOME/rpmbuild\" \
+      -ba ${pckname}.spec
+
+   msg \"testing the rpm installation ...\"
+   rpm --test -hiv ../RPMS/*/*.rpm
+
+   if [ \"'$targetdir'\" ]; then
+      msg \"copying the rpm packages to the target folder ...\"
+      id
+      ls -ld '$targetdir'
+      ls -l '$targetdir'
+      ls -ld /shared
+      ls -l /shared
+      ls -ld /shared/tests-build
+      ls -l /shared/tests-build
+      mkdir -p '$targetdir' &&
+      cp -p ../SRPMS/*.src.rpm ../RPMS/*/*.rpm '$targetdir'
+   fi
+   popd &>/dev/null
+fi
 '"
 
 msg "removing the temporary container ..."
-##container_remove "$container"
+container_remove "$container"
