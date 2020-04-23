3429bd7e269437e6fc330d794e4b641557ca8e57
Davide Madrisan <davide.madrisan@gmail.com>
2020-04-23 23:43:27 +0200

    lib/container_podman: simplify podman_varlink_get()
    Remove the string argument 'param' because the varlink parameter
    can be simply forged by using the function provided by the varlink
    interface 'varlink_object_set_string()'.
    
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- tests/tslibcontainer_podman_list.c
+++ tests/tslibcontainer_podman_list.c
@@ -33,14 +33,14 @@
 #include "units.h"
 
 static int podman_varlink_get (struct podman_varlink *pv,
-			       const char *varlinkmethod, const char *param,
-			       char **json, char **err);
+			       const char *varlinkmethod, char **json,
+			       char **err);
 
 #include "../lib/container_podman_stats.c"
 
 static int
 podman_varlink_get (struct podman_varlink *pv, const char *varlinkmethod,
-		    const char *param, char **json, char **err)
+		    char **json, char **err)
 {
   char *filename = NPL_TEST_PATH_PODMAN_LISTCONTAINERS_JSON;
 
