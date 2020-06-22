9e9c3ed1bd24b3d6ca50e90a110ebbc7257a8fc8
Davide Madrisan <davide.madrisan@gmail.com>
2020-06-22 22:19:37 +0200

    check_network: make it possible to customize the perfdata
    Add some cmdline options for omitting some or all the
    perfdata from the output:
    
      --no-bytes
      --no-collisions
      --no-drops
      --no-errors
      --no-multicast
      --no-packets
    
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- plugins/check_network.c
+++ plugins/check_network.c
@@ -30,16 +30,20 @@
 #else
 # include <linux/rtnetlink.h>
 #endif
+#include <string.h>
 
 #include "common.h"
 #include "messages.h"
 #include "netinfo.h"
 #include "progname.h"
 #include "progversion.h"
+#include "string-macros.h"
 #include "system.h"
 #include "xalloc.h"
 #include "xasprintf.h"
 #include "xstrton.h"
+
+#define MAX_PRINTED_INTERFACES		5
 
 static const char *program_copyright =
   "Copyright (C) 2014,2015,2020 Davide Madrisan <" PACKAGE_BUGREPORT ">\n";
@@ -47,7 +51,13 @@ static const char *program_copyright =
 static struct option const longopts[] = {
   {(char *) "check-link", no_argument, NULL, 'k'},
   {(char *) "ifname", required_argument, NULL, 'i'},
+  {(char *) "no-bytes", no_argument, NULL, 'b'},
+  {(char *) "no-collisions", no_argument, NULL, 'C'},
+  {(char *) "no-drops", no_argument, NULL, 'd'},
+  {(char *) "no-errors", no_argument, NULL, 'e'},
   {(char *) "no-loopback", no_argument, NULL, 'l'},
+  {(char *) "no-multicast", no_argument, NULL, 'm'},
+  {(char *) "no-packets", no_argument, NULL, 'p'},
   {(char *) "no-wireless", no_argument, NULL, 'W'},
   {(char *) "help", no_argument, NULL, GETOPT_HELP_CHAR},
   {(char *) "version", no_argument, NULL, GETOPT_VERSION_CHAR},
@@ -64,14 +74,28 @@ usage (FILE * out)
   fprintf (out, "  %s [-klW] [-i <ifname-regex>] [delay]\n",
 	   program_name);
   fputs (USAGE_OPTIONS, out);
-  fputs ("  -i, --ifname    only display interfaces matching a regular "
+  fputs ("  -i, --ifname         only display interfaces matching a regular "
 	 "expression\n", out);
-  fputs ("  -k, --check-link   report an error if at least a link is down\n",
+  fputs ("  -k, --check-link     report an error if at least a link is down\n",
 	 out);
-  fputs ("  -l, --no-loopback  skip the loopback interface\n", out);
-  fputs ("  -W, --no-wireless  skip the wireless interfaces\n", out);
+  fputs ("  -l, --no-loopback    skip the loopback interface\n", out);
+  fputs ("  -W, --no-wireless    skip the wireless interfaces\n", out);
   fputs (USAGE_HELP, out);
   fputs (USAGE_VERSION, out);
+  fputs ("  By default all the counter are reported but it's possible to "
+	 "select a subset:\n", out);
+  fputs ("  -b  --no-bytes       omit the rx/tx bytes counter from perfdata\n",
+	 out);
+  fputs ("  -c, --no-collisions  omit the collisions counter from perfdata\n",
+	 out);
+  fputs ("  -d  --no-drops       omit the rx/tx drop counters from perfdata\n",
+	 out);
+  fputs ("  -e  --no-errors      omit the rx/tx errors counters "
+	 "from perfdata\n", out);
+  fputs ("  -m, --no-multicast   omit the multicast counter from perfdata\n",
+	 out);
+  fputs ("  -p, --no-packets     omit the rx/tx packets counter from "
+	 "perfdata\n", out);
   fprintf (out, "  delay is the delay between the two network snapshots "
 	   "in seconds (default: %dsec)\n", DELAY_DEFAULT);
   fputs (USAGE_NOTE, out);
@@ -99,22 +123,48 @@ print_version (void)
 int
 main (int argc, char **argv)
 {
-  int c;
+  int c, i, option_index = 0;
   nagstatus status = STATE_OK;
-  char *ifname_regex = NULL;
+  bool pd_bytes = true,
+       pd_collisions = true,
+       pd_drops = true,
+       pd_errors = true,
+       pd_multicast = true,
+       pd_packets = true;
+  char *bp, *ifname_regex = NULL;
+  size_t size;
   unsigned int options = 0;
   unsigned long delay;
+  FILE *perfdata;
 
   set_program_name (argv[0]);
 
   while ((c = getopt_long (argc, argv,
-			   "i:klW" GETOPT_HELP_VERSION_STRING,
-			   longopts, NULL)) != -1)
+			   "Cbdei:klmpW" GETOPT_HELP_VERSION_STRING,
+			   longopts, &option_index)) != -1)
     {
       switch (c)
 	{
 	default:
 	  usage (stderr);
+/*
+	case 0:
+	  if (STREQ (longopts[option_index].name, "no-packets"))
+	    pd_packets = false;
+	  break;
+*/
+	case 'b':
+	  pd_bytes = false;
+	  break;
+	case 'C':
+	  pd_collisions = false;
+	  break;
+	case 'd':
+	  pd_drops = false;
+	  break;
+	case 'e':
+	  pd_errors = false;
+	  break;
 	case 'i':
 	  ifname_regex = xstrdup (optarg);
 	  break;
@@ -123,6 +173,12 @@ main (int argc, char **argv)
 	  break;
 	case 'l':
 	  options |= NO_LOOPBACK;
+	  break;
+	case 'm':
+	  pd_multicast = false;
+	  break;
+	case 'p':
+	  pd_packets = false;
 	  break;
 	case 'W':
 	  options |= NO_WIRELESS;
@@ -152,37 +208,63 @@ main (int argc, char **argv)
   if (ninterfaces < 1)
     status = STATE_UNKNOWN;
 
+  perfdata = open_memstream (&bp, &size);
+
   /* performance data format:
    * 'label'=value[UOM];[warn];[crit];[min];[max] */
-  printf ("%s %s - found %u interface(s) | "
-	  , program_name_short
-	  , state_text (status)
-	  , ninterfaces);
   for (ifl = iflhead; ifl != NULL; ifl = ifl->next)
     {
-      unsigned long long speed = (ifl->speed > 0) ? ifl->speed * 1000*1000/8 : 0;
+      unsigned long long speed =
+ 	(ifl->speed > 0) ? ifl->speed * 1000*1000/8 : 0;
       if (DUPLEX_HALF == ifl->duplex)
 	speed /= 2;
 
       char *perfdata_bytes =
 	(ifl->speed > 0) ? xasprintf (";;;0;%llu", speed) : "";
-      printf ("%s_txpck/s=%u %s_rxpck/s=%u "
-	      "%s_txbyte/s=%u%s %s_rxbyte/s=%u%s "
-	      "%s_txerr/s=%u %s_rxerr/s=%u %s_txdrop/s=%u %s_rxdrop/s=%u "
-	      "%s_mcast/s=%u %s_coll/s=%u "
-	      , ifl->ifname, ifl->tx_packets
-	      , ifl->ifname, ifl->rx_packets
-	      , ifl->ifname, ifl->tx_bytes, perfdata_bytes
-	      , ifl->ifname, ifl->rx_bytes, perfdata_bytes
-	      , ifl->ifname, ifl->tx_errors
-	      , ifl->ifname, ifl->rx_errors
-	      , ifl->ifname, ifl->tx_dropped
-	      , ifl->ifname, ifl->rx_dropped
-	      , ifl->ifname, ifl->multicast
-	      , ifl->ifname, ifl->collisions
-      );
+
+      if (pd_bytes)
+	fprintf (perfdata
+		, "%s_txbyte/s=%u%s %s_rxbyte/s=%u%s "
+		, ifl->ifname, ifl->tx_bytes, perfdata_bytes
+		, ifl->ifname, ifl->rx_bytes, perfdata_bytes);
+      if (pd_errors)
+	fprintf (perfdata
+		, "%s_txerr/s=%u %s_rxerr/s=%u "
+		, ifl->ifname, ifl->tx_errors
+		, ifl->ifname, ifl->rx_errors);
+      if (pd_drops)
+	fprintf (perfdata
+		, "%s_txdrop/s=%u %s_rxdrop/s=%u "
+		, ifl->ifname, ifl->tx_dropped
+		, ifl->ifname, ifl->rx_dropped);
+      if (pd_packets)
+        fprintf (perfdata
+		 , "%s_rxpck/s=%u %s_txpck/s=%u "
+		 , ifl->ifname, ifl->rx_packets
+		 , ifl->ifname, ifl->tx_packets);
+      if (pd_collisions)
+        fprintf (perfdata, "%s_coll/s=%u "
+		 , ifl->ifname, ifl->collisions);
+      if (pd_multicast)
+        fprintf (perfdata, "%s_mcast/s=%u "
+		 , ifl->ifname, ifl->multicast);
     }
-  putchar ('\n');
+
+  fclose (perfdata);
+
+  printf ("%s %s - found %u interface(s) ("
+	  , program_name_short
+	  , state_text (status)
+	  , ninterfaces);
+  for (ifl = iflhead, i=0; ifl != NULL; ifl = ifl->next, i++)
+    if (i < MAX_PRINTED_INTERFACES)
+      printf ("%s%s", i < 1 ? "" : ",", ifl->ifname);
+    else
+      {
+	printf (",...");
+	break;
+      }
+  printf (") | %s\n", bp);
 
   freeiflist (iflhead);
 
